4a2639612363dd2c3e9ce3ed45fda457981b2911
karthikeyan1127 <69983187+karthikeyan1127@users.noreply.github.com>
2020-09-04 01:44:17 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,3 +1,5 @@
+package com.example;
+
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
@@ -17,13 +19,10 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-
-public class Hello implements HttpFunction {
-    @Override
-    public void service(HttpRequest request, HttpResponse response) throws Exception {
-// Authentication is provided by gcloud tool when running locally
-        // and by built-in service accounts when running on GAE, GCE or GKE.
-        Logger Log = LoggerFactory.getLogger(this.getClass());
+public class Example implements HttpFunction {
+  @Override
+  public void service(HttpRequest request, HttpResponse response) throws Exception {
+                    Logger Log = LoggerFactory.getLogger(this.getClass());
         BufferedWriter writer = response.getWriter();
         try {
             Log.info("!");
@@ -48,7 +47,7 @@ public class Hello implements HttpFunction {
 
             // Add your code to assign values to parameters for the 'create' method:
             // * The project which owns the job.
-            String projectId = "tidal-pathway-285009";
+            String projectId = "tidal-pathway-2850029";
 
             RuntimeEnvironment runtimeEnvironment = new RuntimeEnvironment();
             runtimeEnvironment.setBypassTempDirValidation(false);
@@ -58,73 +57,18 @@ public class Hello implements HttpFunction {
             launchTemplateParameters.setEnvironment(runtimeEnvironment);
             launchTemplateParameters.setJobName("newJob" + (new Date()).getTime());
             Map<String, String> params = new HashMap<String, String>();
-            params.put("inputFile", "gs://karthiksfirstbucket/sample");
-            params.put("outputFile", "gs://karthiksfirstbucket/count1");
-            params.put("gcsPath","https://storage.cloud.google.com/pipeline-staging-test/templates/WordCount");
+            params.put("inputFile", "gs://karthiksfirstbucket/sample.txt");
+            params.put("output", "gs://karthiksfirstbucket/count1");
             launchTemplateParameters.setParameters(params);
             writer.write("4");
             Log.info("5646564564645");
             Dataflow.Projects.Templates.Launch launch = dataflowService.projects().templates().launch(projectId, launchTemplateParameters);
             Log.info("56next");
-            launch.setGcsPath("https://storage.cloud.google.com/pipeline-staging-test/templates/WordCount");
+            launch.setGcsPath("gs://dataflow-templates-us-central1/latest/Word_Count");
             launch.execute();
             Log.info("&&&&&&&&&&&&&&&&");
         }catch(Exception e){
             writer.write(e.toString());
         }
-        /*HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-        GoogleCredentials.getApplicationDefault();
-        Dataflow dataflowService = new Dataflow.Builder(httpTransport, jsonFactory, null)
-                .setApplicationName("Google Cloud Platform Sample")
-                .build();
-
-
-        String projectId = "tidal-pathway-285009";
-
-        RuntimeEnvironment runtimeEnvironment = new RuntimeEnvironment();
-        runtimeEnvironment.setBypassTempDirValidation(false);
-        runtimeEnvironment.setTempLocation("gs://karthiksfirstbucket/temp1");
-        runtimeEnvironment.setIpConfiguration("WORKER_IP_UNSPECIFIED");
-        runtimeEnvironment.setAdditionalExperiments(new ArrayList());
-
-        CreateJobFromTemplateRequest createJobFromTemplateRequest = new CreateJobFromTemplateRequest();
-        createJobFromTemplateRequest.setEnvironment(runtimeEnvironment);
-        createJobFromTemplateRequest.setLocation("us-central1");
-        createJobFromTemplateRequest.setGcsPath("gs://pipeline-staging-test/templates/WordCount");
-        createJobFromTemplateRequest.setJobName("karthikssecondjob"+(new Date()).getTime());
-        createJobFromTemplateRequest.setParameters(new HashMap<String,String>());
-        createJobFromTemplateRequest.getParameters().put("inputFile","gs://karthiksfirstbucket/sample.txt");
-        createJobFromTemplateRequest.getParameters().put( "output", "gs://karthiksfirstbucket/count1");
-        Dataflow.Projects.Templates.Create result = dataflowService.projects().templates().create(projectId,createJobFromTemplateRequest);
-
-
-        System.out.println(result.getProjectId());
-    }
-
-    public static void main() throws Exception {
-        String args[] = {"--project=tidal-pathway-285009","--stagingLocation=gs://karthiksfirstbucket/staging/","--output=gs://karthiksfirstbucket/count1","--tempLocation=gs://karthiksfirstbucket/temp1/","--runner=DataflowRunner","--region=us-central1"};
-        Pipeline p = Pipeline.create(
-                PipelineOptionsFactory.fromArgs(args).withValidation().create());
-        p.apply(Create.of("Hello", "World"))
-                .apply(MapElements.via(new SimpleFunction<String, String>() {
-                    @Override
-                    public String apply(String input) {
-                        return input.toUpperCase();
-                    }
-                }))
-                .apply(ParDo.of(new DoFn<String, Void>() {
-                    @ProcessElement
-                    public void processElement(ProcessContext c)  {
-                    }
-                }));
-
-        p.run();
-    }
-    }*/
-    }
-    public static void main(String args[]) throws Exception {
-        Hello hello = new Hello();
-        hello.service(null,null);
-    }
+  }
 }
